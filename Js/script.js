b7ba21120aa0be168145878293869aafe6fd4b4d
wasim8291 <110515548+wasim8291@users.noreply.github.com>
2022-08-30 23:27:12 +0530

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -92,7 +92,24 @@ function generateR(){
 
     document.getElementById("aqT").innerHTML = str1;
 
+    //code for setting image
 
+    let file=document.getElementById("imgField").files[0]
+
+    console.log(file);
+
+    let reader=new FileReader();
+
+    reader.readAsDataURL(file);
+
+    console.log(reader.result);
+
+    //set the image to template
+
+    reader.onloadend = function() {
+      document.getElementById("imgTemplate").src=reader.result;
+    }
+    
     document.getElementById('cv-form').style.display = "none";
     document.getElementById('cv-template').style.display = "block"; 
 }
